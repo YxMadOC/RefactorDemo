5f30f5ec15ced28124cb247c27ba3f9b90e19692
Cayde Xu <cayde.xu@oocl.com>
2019-08-19 22:22:12 +0800

    [GildedRose-Refactor] Replaced if with switch

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -22,46 +22,49 @@ public class Item {
     }
 
     public void updateQuality() {
-        if (name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        }
-        sellIn = sellIn - 1;
 
-        if (name.equals("Aged Brie")) {
-            increaseQualitySafely();
-            if (sellIn < 0) {
-                increaseQualitySafely();
-            }
-        } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            increaseQualitySafely();
-            updatePassedBackstageQuality();
-            if (sellIn < 0) {
-                quality = 0;
-            }
-        } else {
-            if (quality <= 0) {
+        switch (name) {
+            case "Sulfuras, Hand of Ragnaros":
                 return;
-            }
-            quality = quality - 1;
-            if (sellIn < 0) {
-                quality = quality - 1;
-            }
+            case "Aged Brie":
+                decreaseSellIn();
+                increaseQualitySafely();
+                if (sellIn < 0) {
+                    increaseQualitySafely();
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                decreaseSellIn();
+                increaseQualitySafely();
+                if (sellIn < 10) {
+                    increaseQualitySafely();
+                }
+                if (sellIn < 5) {
+                    increaseQualitySafely();
+                }
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+                break;
+            default:
+                decreaseSellIn();
+                if (quality > 0) {
+                    quality = quality - 1;
+                    if (sellIn < 0) {
+                        quality = quality - 1;
+                    }
+                }
+                break;
         }
+    }
+
+    private void decreaseSellIn() {
+        sellIn -= 1;
     }
 
     private void increaseQualitySafely() {
         if (quality < MAX_QUALITY) {
             quality = quality + 1;
-        }
-    }
-
-    private void updatePassedBackstageQuality() {
-        if (sellIn < 10) {
-            increaseQualitySafely();
-        }
-
-        if (sellIn < 5) {
-            increaseQualitySafely();
         }
     }
 
